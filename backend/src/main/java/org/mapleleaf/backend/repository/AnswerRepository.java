f20e806d1ff0763cb42372faaf19c911ceeab669
donggyu <leedong1848@gmail.com>
2022-10-05 22:28:50 +0900

    entity에 대한 repository 추가

--- backend/src/main/java/org/mapleleaf/backend/repository/AnswerRepository.java
+++ backend/src/main/java/org/mapleleaf/backend/repository/AnswerRepository.java
@@ -1,2 +1,9 @@
-package org.mapleleaf.backend.repository;public class AnswerRepository {
+package org.mapleleaf.backend.repository;
+
+import org.mapleleaf.backend.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
--- backend/src/main/java/org/mapleleaf/backend/repository/ProblemRepository.java
+++ backend/src/main/java/org/mapleleaf/backend/repository/ProblemRepository.java
@@ -1,2 +1,9 @@
-package org.mapleleaf.backend.repository;public interface ProblemRepository {
+package org.mapleleaf.backend.repository;
+
+import org.mapleleaf.backend.entity.Problem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
 }
