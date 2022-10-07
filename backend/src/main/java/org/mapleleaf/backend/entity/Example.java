9273011d832d5213847db697276a9edd77302df5
yuri.ji <dbfl9711@gmail.com>
2022-10-07 23:34:57 +0900

    example, problem 테이블 join

--- backend/src/main/java/org/mapleleaf/backend/entity/Example.java
+++ backend/src/main/java/org/mapleleaf/backend/entity/Example.java
@@ -3,10 +3,12 @@ package org.mapleleaf.backend.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Data
 @Entity // db 테이블과 1:1 매핑되는 객체, enum에는 사용 불가
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,15 +17,26 @@ import javax.persistence.*;
 public class Example {
 
     @Id
+    @Column(name="example_id")
     private Long id;
 
-    //@ManyToOne
-//    @Column(name = "problem_id")
-//    private Long problemId;
+    @ManyToOne
+    @JoinColumn(name = "problem_id")
+    private Problem problem;
+
+    public void setProblem(Problem problem){
+        this.problem = problem;
+
+        if(!problem.getExamples().contains(this)){
+            problem.getExamples().add(this);
+        }
+    }
 
     @Column
     private String input;
 
     @Column
     private String output;
+
+
 }
