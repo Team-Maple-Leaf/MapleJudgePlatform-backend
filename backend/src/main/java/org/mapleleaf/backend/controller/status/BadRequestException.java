5fc1383a2951420357f9ed503ed718220379afda
donggyu <leedong1848@gmail.com>
2022-10-09 13:31:44 +0900

    error 처리 관련 exception추가

---
+++ backend/src/main/java/org/mapleleaf/backend/controller/status/BadRequestException.java
@@ -0,0 +1,14 @@
+package org.mapleleaf.backend.controller.status;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String msg) {
+        super(msg);
+    }
+
+    public BadRequestException(){}
+
+}
