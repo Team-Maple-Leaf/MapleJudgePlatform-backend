fbc66d2028ffc50846999da0f36fa534e1e9b972
yuri.ji <dbfl9711@gmail.com>
2022-10-13 01:08:27 +0900

    exception 패키지 경로 수정

---
+++ backend/src/main/java/org/mapleleaf/backend/exception/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package org.mapleleaf.backend.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+@NoArgsConstructor
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String msg) {
+        super(msg);
+    }
+}
