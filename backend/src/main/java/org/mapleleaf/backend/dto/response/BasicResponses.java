6dff8b9e6fb6c1155feaa9b3652c45841d36c93d
yuri.ji <dbfl9711@gmail.com>
2022-10-17 13:58:34 +0900

    Response에서 http_status 제외

--- backend/src/main/java/org/mapleleaf/backend/dto/response/BasicResponses.java
+++ backend/src/main/java/org/mapleleaf/backend/dto/response/BasicResponses.java
@@ -6,15 +6,14 @@ public class BasicResponses {
     static public <T> BasicResponse<T> getOkResponse(String message, T data) {
         return BasicResponse.<T>builder()
                 .code(HttpStatus.OK.value())
-                .httpStatus(HttpStatus.OK)
                 .message(message)
                 .data(data)
                 .build();
     }
+
     static public <T> BasicResponse<T> getNotFoundResponse(String message) {
         return BasicResponse.<T>builder()
                 .code(HttpStatus.NOT_FOUND.value())
-                .httpStatus(HttpStatus.NOT_FOUND)
                 .message(message)
                 .data(null)
                 .build();
@@ -23,7 +22,6 @@ public class BasicResponses {
     static public <T> BasicResponse<T> getBadRequestResponse(String message) {
         return BasicResponse.<T>builder()
                 .code(HttpStatus.BAD_REQUEST.value())
-                .httpStatus((HttpStatus.BAD_REQUEST))
                 .message(message)
                 .data(null)
                 .build();
