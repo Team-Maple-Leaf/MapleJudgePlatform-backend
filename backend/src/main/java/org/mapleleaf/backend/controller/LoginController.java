a5dbaa3c47dd37c45ffb343e2f5b359c44e9fd8e
yuri.ji <dbfl9711@gmail.com>
2022-10-22 22:45:19 +0900

    loginController를 Swagger에 추가

--- backend/src/main/java/org/mapleleaf/backend/controller/LoginController.java
+++ backend/src/main/java/org/mapleleaf/backend/controller/LoginController.java
@@ -1,11 +1,14 @@
 package org.mapleleaf.backend.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.mapleleaf.backend.controller.template.ResponseTemplate;
 import org.mapleleaf.backend.dto.LoginRequestDto;
 import org.mapleleaf.backend.dto.TokenDto;
 import org.mapleleaf.backend.dto.response.BasicResponse;
-import org.mapleleaf.backend.jwt.JwtProvider;
 import org.mapleleaf.backend.service.LoginService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +20,21 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class LoginController {
     private final LoginService loginService;
-    private final JwtProvider jwtProvider;
 
     /* 토큰 생성 컨트롤러 */
+    @ApiOperation(value = "login 요청")
+    @ApiResponses(value = {
+            @ApiResponse(code=401, message="토큰 값이 전달되지 않았습니다"),
+            @ApiResponse(code=401, message="지원하지 않는 JWT 토큰입니다."),
+            @ApiResponse(code=402, message="손상된 JWT 토큰입니다."),
+            @ApiResponse(code=403, message="만료된 JWT 토큰입니다."),
+            @ApiResponse(code=403, message="시그니처 검증에 실패한 토큰입니다.")})
     @PostMapping("/")
-    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto) {
-        if(loginService.isValidRequest(loginRequestDto)) {
-            log.info("유효성 검사 통과");
-            if(!loginService.memberExist(loginRequestDto.getMaple())){
-                log.info("존재하지 않는 멤버");
-                loginService.join(loginRequestDto);
-            }
-
-            String uuid = loginService.getUUID(loginRequestDto.getMaple());
-            String token = jwtProvider.createToken(uuid); // 토큰 생성
-
-            return new ResponseEntity<>(BasicResponse.builder()
-                    .code(HttpStatus.OK.value())
-                    .message("토큰 발급 성공")
-                    .data(new TokenDto(token))
-                    .build(),  HttpStatus.OK);
-        } else {
-            log.info("유효하지 않은 Request");
-            return new ResponseEntity<>(BasicResponse.builder()
-                    .code(HttpStatus.UNAUTHORIZED.value())
-                    .message("유효하지 않은 Request입니다.")
-                    .data("")
-                    .build(),  HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<BasicResponse<TokenDto>> login(@RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseTemplate.execute(
+                "토큰 발급에 성공했습니다.",
+                "유효하지 않은 RequestBody입니다.",
+                () -> loginService.checkRequestAndCreateToken(loginRequestDto),
+                HttpStatus.UNAUTHORIZED);
     }
 }
